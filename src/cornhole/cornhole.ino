c59a04814495e9daf3a2c0fa60ce9bb2f232dd8b
Jason Anderson <jander10@jaguarlandrover.com>
2019-06-22 21:28:29 -0700

    Current

--- src/cornhole/cornhole.ino
+++ src/cornhole/cornhole.ino
@@ -4,11 +4,19 @@
 // variables will change:
 int sensorState = HIGH;
 
+// Break Beam sensor
 #define SENSORPIN 8
-#define NUM_LEDS 10
-CRGB leds[NUM_LEDS];
 
-#include <LiquidCrystal.h>
+// LED Defines
+#define LED_SENSOR 13
+#define NUM_LEDS 119
+#define FRAME_START 0
+#define FRAME_END 101
+#define RING_START 102
+#define RING_END 118
+
+
+CHSV leds[ NUM_LEDS ];
 
 // Init LCD
 const int rs = 7, en = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
@@ -19,85 +27,83 @@ int blue_score = 0;
 int red_score = 0;
 
 // Button struct
-typedef struct {
+typedef struct
+{
   const int pin;
   int state;
-  void (*func)(int*);
+  void (*func)(int *);
 } button_t;
 
 void setDisplay();
 
-void scoreUp(int* a) { ++*a; printScore(); };
-void scoreDown(int* a) { --*a; printScore(); };
+void scoreUp(int *a)
+{
+  ++*a;
+  printScore();
+};
+void scoreDown(int *a)
+{
+  --*a;
+  printScore();
+};
 
 #define NUM_BUTTONS 4
 button_t buttons[NUM_BUTTONS] = {
-  {9,  0, scoreUp},
-  {10, 0, scoreDown},
-  {11, 0, scoreUp},
-  {12, 0, scoreDown}
-};
+    {9, 0, scoreUp},
+    {10, 0, scoreDown},
+    {11, 0, scoreUp},
+    {12, 0, scoreDown}};
 
-void checkButtons() {
-  for(int i = 0; i < NUM_BUTTONS; ++i)
+void checkButtons()
+{
+  for (int i = 0; i < NUM_BUTTONS; ++i)
   {
     int bs = digitalRead(buttons[i].pin);
-    if (bs == HIGH && buttons[i].state == 0) {
-      buttons[i].func( (i<2)?(&red_score):(&blue_score) );
+    if (bs == HIGH && buttons[i].state == 0)
+    {
+      buttons[i].func((i < 2) ? (&red_score) : (&blue_score));
       buttons[i].state = 1;
     }
-    else if ( bs == LOW ) {
+    else if (bs == LOW)
+    {
       buttons[i].state = 0;
     }
   }
-
 }
 
-void printScore() {
-  if ( blue_score < 0 ) 
+void printScore()
+{
+  if (blue_score < 0)
     blue_score = 0;
-  
-  if ( red_score < 0 )
+
+  if (red_score < 0)
     red_score = 0;
 
-  lcd.setCursor(0, 0); 
+  lcd.setCursor(0, 0);
   String str = "Red:    " + String(red_score);
   lcd.print(str);
   Serial.print(str);
 
-  lcd.setCursor(0, 1); 
+  lcd.setCursor(0, 1);
   str = "Blue:   " + String(blue_score);
   Serial.print(str);
   lcd.print(str);
   lcd.display();
 }
 
-void iterLightShow(uint8_t hue)
+void setLEDState(float percent, uint8_t hue)
 {
-    for (int i = 0; i < NUM_LEDS; i++)
-    {
-      // Set the i'th led to red
-      leds[i] = CHSV(hue, 255, 255);
-      if ((i + 1) < NUM_LEDS)
-        leds[i + 1] = CHSV(hue, 255, 180);
-      if ((i + 2) < NUM_LEDS)
-        leds[i + 2] = CHSV(hue, 255, 80);
+  uint8_t offset = 0;
+  fill_solid(leds, NUM_LEDS, CHSV(0,0,0) );
+  
 
-      for (int j = 0; j < NUM_LEDS; j++)
-      {
-        if (j != i && j != i + 1 && j != i + 2)
-          leds[j] = CHSV(0, 255, 0);
-      }
-
-      // Show the leds
-      FastLED.show();
-      delay(20);
-    }
+  // Show the leds
+  FastLED.show();
 }
 
 void setup()
 {
-  FastLED.addLeds<NEOPIXEL, 13>(leds, NUM_LEDS);
+  FastLED.addLeds<NEOPIXEL, LED_SENSOR>(leds, NUM_LEDS);
 
   // Init break-beam pins
   pinMode(SENSORPIN, INPUT);
@@ -105,12 +111,12 @@ void setup()
 
   // Start serial output
   Serial.begin(9600);
-  
+
   // Setup button pins
   pinMode(buttons[0].pin, INPUT);
   pinMode(buttons[1].pin, INPUT);
   pinMode(buttons[2].pin, INPUT);
-  pinMode(buttons[3].pin, INPUT); 
+  pinMode(buttons[3].pin, INPUT);
 
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
@@ -118,25 +124,23 @@ void setup()
   Serial.print("Setup...\n");
 }
 
-
 // Sets all LEDs to white
 void setWhite()
-{    
-    for (int i = 0; i < NUM_LEDS; i++)
-    {
-      leds[i] = (CRGB::White);
-    }
-    FastLED.show();
+{
+  for (int i = 0; i < NUM_LEDS; i++)
+  {
+    leds[i] = (CRGB::White);
+  }
+  FastLED.show();
 }
-
-
 
 #define HOLED_TIME 2000
 
 bool beamBroken = false;
 bool lightShow = false;
 
-enum STATES {
+enum STATES
+{
   PLAYING,
   HOLED,
   VICTORY,
@@ -151,34 +155,38 @@ void loop()
 {
   // read the state of the pushbutton value:
   sensorState = digitalRead(SENSORPIN);
-  switch(currentState){
-    case PLAYING:
+  switch (currentState)
+  {
+  case PLAYING:
+  {
+    if (!beamBroken && (sensorState == LOW))
     {
-      if( !beamBroken && (sensorState == LOW) )
-      {
-        currentState = HOLED;     
-        beamBroken = true;  
-        stateStart = millis();
-        Serial.print("Entering HOLED state...\n");
-      }
+      currentState = HOLED;
+      beamBroken = true;
+      stateStart = millis();
+      Serial.print("Entering HOLED state...\n");
     }
-    break;
-    case HOLED:
+  }
+  break;
+  case HOLED:
+  {
+    // iterLightShow();
+    unsigned long timeFromStart = millis() - stateStart;
+    setLEDState((float)timeFromStart / (float)HOLED_TIME)
+
+    if ( timeFromStart >= HOLED_TIME)
     {
-     // iterLightShow();
-      if( (millis()-stateStart) >= HOLED_TIME )
-      {
-        Serial.print("Entering PLAYING state...\n");
-        currentState = PLAYING;
-        beamBroken = false; 
-      }
+      Serial.print("Entering PLAYING state...\n");
+      currentState = PLAYING;
+      beamBroken = false;
     }
-    break;
-    case VICTORY:
-    {
-      // if ( (blue_score >= 21) || (red_score >= 21) )
-    }
-    break;
+  }
+  break;
+  case VICTORY:
+  {
+    // if ( (blue_score >= 21) || (red_score >= 21) )
+  }
+  break;
   }
   checkButtons();
 }
